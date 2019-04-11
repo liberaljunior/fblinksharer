6b13cad80770aa966b016a1bb13babc6f7d49fca
majolelo <majolelo@live.com>
2019-04-11 17:12:53 +0700

    Adding feature to detect more 1 URLs and select which one is desired

--- app/build/gen/com/majolelo/fblinksharer/R.java
+++ app/build/gen/com/majolelo/fblinksharer/R.java
@@ -18,15 +18,24 @@ public final class R {
     }
     public static final class id {
         public static final int btnshare=0x7f070001;
+        public static final int btnurl1=0x7f070002;
+        public static final int btnurl2=0x7f070004;
         public static final int txturl=0x7f070000;
+        public static final int txturl1=0x7f070003;
+        public static final int txturl2=0x7f070005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int popup_more1=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int empty=0x7f040006;
         public static final int hello_world=0x7f040001;
+        public static final int more1=0x7f040003;
         public static final int myurl_hint=0x7f040002;
+        public static final int url=0x7f040005;
+        public static final int usethis=0x7f040004;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
--- app/src/main/java/com/majolelo/fblinksharer/MainActivity.java
+++ app/src/main/java/com/majolelo/fblinksharer/MainActivity.java
@@ -9,54 +9,109 @@ import android.view.*;
 import android.widget.*;
 import java.util.*;
 import java.util.regex.*;
+import org.apache.http.util.*;
+import android.view.View.*;
 
 public class MainActivity extends Activity 
 {
+	final Context context=this;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-				Intent i=getIntent();
-				String action=i.getAction();
-				String type=i.getType();
-				if (Intent.ACTION_SEND.equals(action) && type != null)
-				{
-						if ("text/plain".equals(type))
-						{
-								handleSentText(i);
-						}
-				}
+		Intent i=getIntent();
+		String action=i.getAction();
+		String type=i.getType();
+		if (Intent.ACTION_SEND.equals(action) && type != null)
+		{
+			if ("text/plain".equals(type))
+			{
+				handleSentText(i);
+			}
+		}
     }
-		public void shareToFB(View view)
+	public void shareToFB(View view)
+	{
+		String url="https://m.facebook.com/sharer.php?u=";
+		EditText txturl= (EditText) findViewById(R.id.txturl);
+		CharSequence userurl=txturl.getText();
+		String userurlstring=txturl.getText().toString();
+		if (userurlstring.isEmpty())
 		{
-				String url="https://m.facebook.com/sharer.php?u=";
-				EditText txturl= (EditText) findViewById(R.id.txturl);
-				CharSequence userurl=txturl.getText();
-				List<String> urls = extractUrls(userurl.toString());
-				url += java.net.URLEncoder.encode(urls.get(0).toString());
-				Intent i=new Intent(Intent.ACTION_VIEW);
-				i.setData(Uri.parse(url));
-				startActivity(i);
+			Toast toast=Toast.makeText(getApplicationContext(), getString(R.string.empty), Toast.LENGTH_LONG);
+			toast.show();
+			return;
 		}
-		void handleSentText(Intent i)
+		List<String> urls = extractUrls(userurl.toString());
+		if (urls.size() > 1)
 		{
-				String sentText=i.getStringExtra(Intent.EXTRA_TEXT);
-				EditText txtUrl=(EditText) findViewById(R.id.txturl);
-				txtUrl.setText(sentText, TextView.BufferType.EDITABLE);
+			showDiaForm(urls);
 		}
-		public static List<String> extractUrls(String input){
-				List<String> result = new ArrayList<String>();
-				String[] words = input.split("\\s+");
-				Pattern pattern = Patterns.WEB_URL;
-				for(String word : words){
-						if(pattern.matcher(word).find()){
-								if(!word.toLowerCase().contains("http://") && !word.toLowerCase().contains("https://")){
-										word="http://"+word;
-								}
-								result.add(word);
-						}
+		else
+		{
+			url += java.net.URLEncoder.encode(urls.get(0).toString());
+			Intent i=new Intent(Intent.ACTION_VIEW);
+			i.setData(Uri.parse(url));
+			startActivity(i);
+		}
+	}
+	void handleSentText(Intent i)
+	{
+		String sentText=i.getStringExtra(Intent.EXTRA_TEXT);
+		EditText txtUrl=(EditText) findViewById(R.id.txturl);
+		txtUrl.setText(sentText, TextView.BufferType.EDITABLE);
+	}
+	public static List<String> extractUrls(String input)
+	{
+		List<String> result = new ArrayList<String>();
+		String[] words = input.split("\\s+");
+		Pattern pattern = Patterns.WEB_URL;
+		for (String word : words)
+		{
+			if (pattern.matcher(word).find())
+			{
+				if (!word.toLowerCase().contains("http://") && !word.toLowerCase().contains("https://"))
+				{
+					word = "http://" + word;
 				}
-				return result;
+				result.add(word);
+			}
 		}
+		return result;
+	}
+	private void showDiaForm(List urls)
+	{
+		final Dialog dialog=new Dialog(context);
+		dialog.setContentView(R.layout.popup_more1);
+		dialog.setTitle(getText(R.string.more1));
+		final TextView text=(TextView) dialog.findViewById((R.id.txturl1));
+		text.setText(urls.get(0).toString());
+		final TextView tex2=(TextView) dialog.findViewById((R.id.txturl2));
+		tex2.setText(urls.get(1).toString());
+
+		final EditText txtUrl=(EditText) findViewById(R.id.txturl);
+
+		Button btnurl1=(Button) dialog.findViewById(R.id.btnurl1);
+		btnurl1.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v)
+				{
+					txtUrl.setText(text.getText());
+					dialog.dismiss();
+				}
+			});
+
+		Button btnurl2=(Button) dialog.findViewById(R.id.btnurl2);
+		btnurl2.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v)
+				{
+					txtUrl.setText(tex2.getText());
+					dialog.dismiss();
+				}
+			});
+
+		dialog.show();
+	}
 }
