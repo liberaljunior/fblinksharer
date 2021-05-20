4bbc8feae0e0a0d41c9c5147d8f5f87d260647f7
M. Wilson <majolelo@live.com>
2021-05-20 14:38:29 +0800

    App to remove from memory after clicking 'Share to Facebook'

--- app/build/gen/com/majolelo/fblinksharer/BuildConfig.java
+++ app/build/gen/com/majolelo/fblinksharer/BuildConfig.java
@@ -2,5 +2,5 @@
 package com.majolelo.fblinksharer;
 
 public final class BuildConfig {
-    public final static boolean DEBUG = true;
+    public final static boolean DEBUG = false;
 }
--- app/src/main/java/com/majolelo/fblinksharer/MainActivity.java
+++ app/src/main/java/com/majolelo/fblinksharer/MainActivity.java
@@ -14,132 +14,134 @@ import android.view.View.*;
 
 public class MainActivity extends Activity 
 {
-	final Context context=this;
+		final Context context=this;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-		Intent i=getIntent();
-		String action=i.getAction();
-		String type=i.getType();
-		if (Intent.ACTION_SEND.equals(action) && type != null)
-		{
-			if ("text/plain".equals(type))
-			{
-				handleSentText(i);
-			}
-		}
+				Intent i=getIntent();
+				String action=i.getAction();
+				String type=i.getType();
+				if (Intent.ACTION_SEND.equals(action) && type != null)
+				{
+						if ("text/plain".equals(type))
+						{
+								handleSentText(i);
+						}
+				}
     }
-	public void shareToFB(View view)
-	{
-		String url="https://m.facebook.com/sharer.php?u=";
-		EditText txturl= (EditText) findViewById(R.id.txturl);
-		CharSequence userurl=txturl.getText();
-		String userurlstring=txturl.getText().toString();
-		if (userurlstring.isEmpty())
+		public void shareToFB(View view)
 		{
-			Toast toast=Toast.makeText(getApplicationContext(), getString(R.string.empty), Toast.LENGTH_LONG);
-			toast.show();
-			return;
-		}
-		List<String> urls = extractUrls(userurl.toString());
-		if (urls.size() > 1)
-		{
-			showDiaForm(urls);
-		}
-		else
-		{
-			url += java.net.URLEncoder.encode(urls.get(0).toString());
-			Intent i=new Intent(Intent.ACTION_VIEW);
-			i.setData(Uri.parse(url));
-			startActivity(i);
-		}
-	}
-	void handleSentText(Intent i)
-	{
-		String sentText=i.getStringExtra(Intent.EXTRA_TEXT);
-		EditText txtUrl=(EditText) findViewById(R.id.txturl);
-		txtUrl.setText(sentText, TextView.BufferType.EDITABLE);
-	}
-	public static List<String> extractUrls(String input)
-	{
-		List<String> result = new ArrayList<String>();
-		String[] words = input.split("\\s+");
-		Pattern pattern = Patterns.WEB_URL;
-		for (String word : words)
-		{
-			if (pattern.matcher(word).find())
-			{
-				if (!word.toLowerCase().contains("http://") && !word.toLowerCase().contains("https://"))
+				String url="https://m.facebook.com/sharer.php?u=";
+				EditText txturl= (EditText) findViewById(R.id.txturl);
+				CharSequence userurl=txturl.getText();
+				String userurlstring=txturl.getText().toString();
+				if (userurlstring.isEmpty())
 				{
-					word = "http://" + word;
+						Toast toast=Toast.makeText(getApplicationContext(), getString(R.string.empty), Toast.LENGTH_LONG);
+						toast.show();
+						return;
 				}
-				result.add(word);
-			}
+				List<String> urls = extractUrls(userurl.toString());
+				if (urls.size() > 1)
+				{
+						showDiaForm(urls);
+				}
+				else
+				{
+						url += java.net.URLEncoder.encode(urls.get(0).toString());
+						Intent i=new Intent(Intent.ACTION_VIEW);
+						i.setData(Uri.parse(url));
+						startActivity(i);
+						finish();
+				}
 		}
-		return result;
-	}
-	private void showDiaForm(List urls)
-	{
-		final Dialog dialog=new Dialog(context);
-		dialog.setContentView(R.layout.popup_more1);
-		dialog.setTitle(getText(R.string.more1));
-		final TextView text=(TextView) dialog.findViewById((R.id.txturl1));
-		text.setText(urls.get(0).toString());
-		final TextView tex2=(TextView) dialog.findViewById((R.id.txturl2));
-		tex2.setText(urls.get(1).toString());
-
-		final EditText txtUrl=(EditText) findViewById(R.id.txturl);
-
-		Button btnurl1=(Button) dialog.findViewById(R.id.btnurl1);
-		btnurl1.setOnClickListener(new OnClickListener(){
-				@Override
-				public void onClick(View v)
-				{
-					txtUrl.setText(text.getText());
-					dialog.dismiss();
-				}
-			});
-
-		Button btnurl2=(Button) dialog.findViewById(R.id.btnurl2);
-		btnurl2.setOnClickListener(new OnClickListener(){
-				@Override
-				public void onClick(View v)
-				{
-					txtUrl.setText(tex2.getText());
-					dialog.dismiss();
-				}
-			});
-
-		dialog.show();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		MenuInflater inf=getMenuInflater();
-		inf.inflate(R.menu.optionmenu, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		if (item.getItemId() == R.id.about)
+		void handleSentText(Intent i)
 		{
-			final Dialog dialog=new Dialog(context);
-			
-			dialog.setContentView(R.layout.about);
-			Button diaBtn=(Button) dialog.findViewById(R.id.btnokabout);
-			diaBtn.setOnClickListener(new OnClickListener(){
-				@Override
-				public void onClick(View v){
-					dialog.dismiss();
-				}
-			});
-			dialog.show();
+				String sentText=i.getStringExtra(Intent.EXTRA_TEXT);
+				EditText txtUrl=(EditText) findViewById(R.id.txturl);
+				txtUrl.setText(sentText, TextView.BufferType.EDITABLE);
 		}
-		return super.onOptionsItemSelected(item);
-	}
+		public static List<String> extractUrls(String input)
+		{
+				List<String> result = new ArrayList<String>();
+				String[] words = input.split("\\s+");
+				Pattern pattern = Patterns.WEB_URL;
+				for (String word : words)
+				{
+						if (pattern.matcher(word).find())
+						{
+								if (!word.toLowerCase().contains("http://") && !word.toLowerCase().contains("https://"))
+								{
+										word = "http://" + word;
+								}
+								result.add(word);
+						}
+				}
+				return result;
+		}
+		private void showDiaForm(List urls)
+		{
+				final Dialog dialog=new Dialog(context);
+				dialog.setContentView(R.layout.popup_more1);
+				dialog.setTitle(getText(R.string.more1));
+				final TextView text=(TextView) dialog.findViewById((R.id.txturl1));
+				text.setText(urls.get(0).toString());
+				final TextView tex2=(TextView) dialog.findViewById((R.id.txturl2));
+				tex2.setText(urls.get(1).toString());
+
+				final EditText txtUrl=(EditText) findViewById(R.id.txturl);
+
+				Button btnurl1=(Button) dialog.findViewById(R.id.btnurl1);
+				btnurl1.setOnClickListener(new OnClickListener(){
+								@Override
+								public void onClick(View v)
+								{
+										txtUrl.setText(text.getText());
+										dialog.dismiss();
+								}
+						});
+
+				Button btnurl2=(Button) dialog.findViewById(R.id.btnurl2);
+				btnurl2.setOnClickListener(new OnClickListener(){
+								@Override
+								public void onClick(View v)
+								{
+										txtUrl.setText(tex2.getText());
+										dialog.dismiss();
+								}
+						});
+
+				dialog.show();
+		}
+
+		@Override
+		public boolean onCreateOptionsMenu(Menu menu)
+		{
+				MenuInflater inf=getMenuInflater();
+				inf.inflate(R.menu.optionmenu, menu);
+				return super.onCreateOptionsMenu(menu);
+		}
+
+		@Override
+		public boolean onOptionsItemSelected(MenuItem item)
+		{
+				if (item.getItemId() == R.id.about)
+				{
+						final Dialog dialog=new Dialog(context);
+
+						dialog.setContentView(R.layout.about);
+						Button diaBtn=(Button) dialog.findViewById(R.id.btnokabout);
+						diaBtn.setOnClickListener(new OnClickListener(){
+										@Override
+										public void onClick(View v)
+										{
+												dialog.dismiss();
+										}
+								});
+						dialog.show();
+				}
+				return super.onOptionsItemSelected(item);
+		}
 }
